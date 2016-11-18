13a5208ba4c8c7c4399040a8805fc40b0f694878
Abanoub <abanoubwagih@gmail.com>
2016-11-18 15:57:59 +0200

    handle some bugs

--- app/src/main/java/com/gmail/abanoub/mymal_popularmovies/setting/SettingActivity.java
+++ app/src/main/java/com/gmail/abanoub/mymal_popularmovies/setting/SettingActivity.java
@@ -1,8 +1,6 @@
 package com.gmail.abanoub.mymal_popularmovies.setting;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.gmail.abanoub.mymal_popularmovies.R;
@@ -16,9 +14,5 @@ public class SettingActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction().replace(R.id.fragment_setting,new SettingFragment()).commit();
     }
 
-    @Nullable
-    @Override
-    public Intent getParentActivityIntent() {
-        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    }
+
 }
